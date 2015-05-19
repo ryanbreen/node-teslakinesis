a66d903a2c7dcecbce1f1929479ada2ea9ed38a2
Ryan Breen <rbreen@cimpress.com>
2015-05-18 21:55:33 -0400

    Write to dynamo.

--- lambda/index.js
+++ lambda/index.js
@@ -1,10 +1,17 @@
-console.log('Loading function');
-
+var AWS = require('aws-sdk');
+var ddb = new AWS.DynamoDB();
 exports.handler = function(event, context) {
     console.log(JSON.stringify(event, null, 2));
     event.Records.forEach(function(record) {
         // Kinesis data is base64 encoded so decode here
-        payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+        payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
+        ddb.putItem({
+          Item: payload,
+          TableName: 'tesla'
+        }, function(err, data) {
+          if (err) return console.error('Failed to write to dynamo due to %s', err);
+          console.log('wrote to dynamo: %s', data);
+        });
         console.log('Decoded payload:', payload);
     });
     context.succeed();
