5c91bdef3100c0da6f0e5e0ee72ba40397ef1afc
Ryan Breen <rbreen@cimpress.com>
2015-05-18 22:44:19 -0400

    Set all fields.

--- lambda/index.js
+++ lambda/index.js
@@ -4,9 +4,24 @@ exports.handler = function(event, context) {
     event.Records.forEach(function(record) {
         // Kinesis data is base64 encoded so decode here
         payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
+
+        //["timestamp","speed","odometer","soc","elevation","est_heading","est_lat","est_lng","power","shift_state","range","est_range","heading"]
+
         ddb.putItem({
           Item: {
-            'timestamp': { 'N': payload['timestamp'] }
+            'timestamp': { 'N': payload['timestamp'] },
+            'speed': { 'N': payload['speed'] ? payload['speed'] : -1 },
+            'odometer': { 'N': payload['odometer'] },
+            'soc': { 'N': payload['soc'] },
+            'elevation': { 'N': payload['elevation'] },
+            'est_heading': { 'N': payload['est_heading'] },
+            'est_lat': { 'N': payload['est_lat'] },
+            'est_lng': { 'N': payload['est_lng'] },
+            'power': { 'N': payload['power'] },
+            'shift_state': { 'S': payload['shift_state'] },
+            'range': { 'N': payload['range'] },
+            'est_range': { 'N': payload['est_range'] },
+            'heading': { 'N': payload['heading'] }
           },
           TableName: 'tesla'
         }, function(err, data) {
