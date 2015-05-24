f8bee08a09bfd1c1847afbecc13adc086b33c370
Ryan Breen <rbreen@cimpress.com>
2015-05-24 13:02:29 -0400

    Convert to writing to postgres from lambda.

---
+++ lambda/Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lambda_invoke: {
+        default: {
+            options: {
+            }
+        }
+    },
+    lambda_deploy: {
+      default: {
+        arn: 'arn:aws:lambda:us-east-1:465070256155:function:tesla_data_insert'
+      }
+    },
+    lambda_package: {
+      default: {
+      }
+    }
+  });
+
+  // Load the plugin that provides the "lambda" tasks.
+  grunt.loadNpmTasks('grunt-aws-lambda');
+
+  // Default task(s).
+  grunt.registerTask('default', ['lambda_package','lambda_deploy']);
+
+};
--- lambda/index.js
+++ lambda/index.js
@@ -1,33 +1,46 @@
 var AWS = require('aws-sdk');
+
+var INSERT_METRIC =
+  "INSERT INTO vehicle_telemetry_metrics(id, timestamp, speed, odometer, soc, elevation, est_heading, heading, location, power, shift_state, range, est_range) " +
+                                "VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);"
+
 exports.handler = function(event, context) {
+
+  // connect to DB
+  var pg = require('pg');
+  var conn_string = "postgres://wrb:" + process.env.DB_PASSWORD + "@tesla.cfwzoyel2syn.us-east-1.rds.amazonaws.com/tesla";
+
+  pg.connect(conn_string, function(err, client, done) {
+
+    if (err) return context.fail(err);
+
     event.Records.forEach(function(record) {
-        // Kinesis data is base64 encoded so decode here
-        payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
+      // Kinesis data is base64 encoded so decode here
+      payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
 
-        //["timestamp","speed","odometer","soc","elevation","est_heading","est_lat","est_lng","power","shift_state","range","est_range","heading"]
+      //["timestamp","speed","odometer","soc","elevation","est_heading","est_lat","est_lng","power","shift_state","range","est_range","heading"]
+      console.log(new Date(parseInt(payload['timestamp'])).toISOString());
 
-        ddb.putItem({
-          Item: {
-            'id': { 'S': payload['usersha'] + '_' + payload['timestamp'] },
-            'timestamp': { 'N': payload['timestamp'] },
-            'speed': { 'N': payload['speed'] && payload['speed'] !== '' ? payload['speed'] : '-1' },
-            'odometer': { 'N': payload['odometer'] },
-            'soc': { 'N': payload['soc'] },
-            'elevation': { 'N': payload['elevation'] },
-            'est_heading': { 'N': payload['est_heading'] },
-            'est_lat': { 'N': payload['est_lat'] },
-            'est_lng': { 'N': payload['est_lng'] },
-            'power': { 'N': payload['power'] },
-            'shift_state': { 'S': payload['shift_state'] && payload['shift_state'] !== '' ? payload['shift_state'] : 'O' },
-            'range': { 'N': payload['range'] },
-            'est_range': { 'N': payload['est_range'] },
-            'heading': { 'N': payload['heading'] }
-          },
-          TableName: 'tesla_vehicle_status'
-        }, function(err, data) {
-          if (err) return context.fail(err);
-          console.log('wrote to dynamo: %s', data);
-          context.succeed();
-        });
+      client.query(INSERT_METRIC, [
+          new Date(parseInt(payload['timestamp'])).toISOString(),
+          (payload['speed'] && payload['speed'] !== '' ? payload['speed'] : '-1' ),
+          payload['odometer'],
+          payload['soc'],
+          payload['elevation'],
+          payload['est_heading'],
+          payload['heading'],
+          //'POINT(' + payload['est_lat'] + ' ' + payload['est_lng'] + ')',
+          "(" + payload['est_lat'] + ',' + payload['est_lng'] + ")",
+          payload['power'],
+          (payload['shift_state'] && payload['shift_state'] !== '' ? payload['shift_state'] : 'O'),
+          payload['range'],
+          payload['est_range']
+        ],
+      function(err, result) {
+        if (err) return context.fail(err);
+        console.log('wrote to postgres: %s', result);
+        context.succeed();
+      });
     });
+  });
 };
