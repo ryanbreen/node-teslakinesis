4ae005062977afd892a2423d017fae9dca6cdf83
Ryan Breen <rbreen@cimpress.com>
2015-06-02 13:55:49 -0400

    Still flailing at working tests.

--- ui/tesla/test/models/vehicle_telemetry_metric_test.rb
+++ ui/tesla/test/models/vehicle_telemetry_metric_test.rb
@@ -3,6 +3,24 @@ require 'test_helper'
 class VehicleTelemetryMetricTest < ActiveSupport::TestCase
   test "close metrics" do
 
+    far_point = VehicleTelemetryMetric.create!(
+      timestamp:      1,
+      vehicle_id:     "1001", 
+      location:       factory.point(-77.000000, 40.000000)
+    )
+    close_point = VehicleTelemetryMetric.create!(
+      timestamp:      2,
+      vehicle_id:     "1001", 
+      location:       factory.point(-75.990000, 39.010000)
+    )
+
+    #all_vehicles = VehicleTelemetryMetric.all
+    #all_vehicles.each do |key|
+    #  puts key
+    #end
+
+#    puts "String format: #{RGeo::WKRep::WKBGenerator.new(hex_format: true, type_format: :ewkb, emit_ewkb_srid: true).generate(RGeo::Geographic.spherical_factory(srid: 4326).point(-77.000000, 40.000000))}"
+
     assert_equal 2,           VehicleTelemetryMetric.all.size
 
     # This returns a value from the DB but not from here.  Why?
