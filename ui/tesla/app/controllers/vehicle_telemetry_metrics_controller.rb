90c1323311738696ddbc8b17a14795a8ea7f02cf
Ryan Breen <rbreen@cimpress.com>
2015-06-03 17:00:10 -0400

    Add a proper bounding box for polylines.

--- ui/tesla/app/controllers/vehicle_telemetry_metrics_controller.rb
+++ ui/tesla/app/controllers/vehicle_telemetry_metrics_controller.rb
@@ -2,11 +2,21 @@ class VehicleTelemetryMetricsController < ApplicationController
 
   def index
     @vehicle_telemetry_metrics = VehicleTelemetryMetric.where("vehicle_id = ?", params[:id])
-    
+
+    lowest_lng, highest_lng, lowest_lat, highest_lat = nil
+
     @hash = Gmaps4rails.build_markers(@vehicle_telemetry_metrics) do |vehicle, marker|
+      lowest_lat = vehicle.location.latitude if !lowest_lat || vehicle.location.latitude < lowest_lat
+      lowest_lng = vehicle.location.longitude if !lowest_lng || vehicle.location.longitude < lowest_lng
+      highest_lat = vehicle.location.latitude if !highest_lat || vehicle.location.latitude > highest_lat
+      highest_lng = vehicle.location.longitude if !highest_lng || vehicle.location.longitude > highest_lat
+
       marker.lat vehicle.location.latitude
       marker.lng vehicle.location.longitude
     end
+
+    @upper_left = { :lat => highest_lat, :lng => lowest_lng }
+    @lower_right = { :lat => lowest_lat, :lng => highest_lng }
   end
 
 
