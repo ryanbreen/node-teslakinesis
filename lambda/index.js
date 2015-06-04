64e4a129131e86df6b81f64bd80f5d4d43db7f3a
Ryan Breen <rbreen@cimpress.com>
2015-06-04 11:11:22 -0400

    Switch lambda function to use underscore.

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,7 @@
 var AWS = require('aws-sdk');
 var crypto = require('crypto');
 var pg = require('pg');
+var _ = require('underscore');
 
 var INSERT_METRIC =
   "INSERT INTO vehicle_telemetry_metrics(id, vehicle_id, timestamp, speed, odometer, soc, elevation, est_heading, heading, location, power, shift_state, range, est_range, created_at, updated_at) " +
@@ -18,55 +19,55 @@ exports.handler = function(event, context) {
 
     if (err) return context.fail(err);
 
-    var countdown = event.Records.length;
-
-    // TODO: Replace this with some simple underscore magic.
-    var process_records = function() {
-      countdown -= 1;
-
-      if (countdown <= 0) {
-        context.succeed();
-      }
-    };
+    // Call context succeed after this function has been called once per record
+    var process_records = _.after(event.Records.length, function() {
+      context.succeed();
+    });
 
     event.Records.forEach(function(record) {
-      // Kinesis data is base64 encoded so decode here
-      payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
 
-      // Short-circuit if record doesn't look well-formed
-      if (isNaN(parseFloat(payload['odometer']))) return process_records();
+      try {
+        // Kinesis data is base64 encoded so decode here
+        payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
 
-      console.log(require('util').inspect(payload));
+        // Short-circuit if record doesn't look well-formed
+        if (isNaN(parseFloat(payload['odometer']))) return process_records();
 
-      //["timestamp","speed","odometer","soc","elevation","est_heading","est_lat","est_lng","power","shift_state","range","est_range","heading"]
-      console.log(new Date(parseInt(payload['timestamp'])).toISOString());
+        console.log(require('util').inspect(payload));
 
-      var now = new Date().toISOString();
+        //["timestamp","speed","odometer","soc","elevation","est_heading","est_lat","est_lng","power","shift_state","range","est_range","heading"]
+        console.log(new Date(parseInt(payload['timestamp'])).toISOString());
 
-      client.query(INSERT_METRIC, [
-          payload['vehicle_id'],
-          new Date(parseInt(payload['timestamp'])).toISOString(),
-          (payload['speed'] && payload['speed'] !== '' ? payload['speed'] : '-1' ),
-          payload['odometer'],
-          payload['soc'],
-          payload['elevation'],
-          payload['est_heading'],
-          payload['heading'],
-          //"(" + payload['est_lat'] + ',' + payload['est_lng'] + ")",
-          "POINT(" + payload['est_lng'] + ' ' + payload['est_lat'] + " " + payload['elevation'] + ")",
-          // "(" + payload['est_lat'] + ',' + payload['est_lng'] + ',' + payload['elevation'] + ")",
-          payload['power'],
-          (payload['shift_state'] && payload['shift_state'] !== '' ? payload['shift_state'] : 'O'),
-          payload['range'],
-          payload['est_range'],
-          now,
-          now
-        ],
-      function(err, result) {
-        if (err) console.error('Write failed due to %s', err);
-        else console.log('wrote to postgres: %s', result);
+        var now = new Date().toISOString();
+
+        client.query(INSERT_METRIC, [
+            payload['vehicle_id'],
+            new Date(parseInt(payload['timestamp'])).toISOString(),
+            (payload['speed'] && payload['speed'] !== '' ? payload['speed'] : '-1' ),
+            payload['odometer'],
+            payload['soc'],
+            payload['elevation'],
+            payload['est_heading'],
+            payload['heading'],
+            //"(" + payload['est_lat'] + ',' + payload['est_lng'] + ")",
+            "POINT(" + payload['est_lng'] + ' ' + payload['est_lat'] + " " + payload['elevation'] + ")",
+            // "(" + payload['est_lat'] + ',' + payload['est_lng'] + ',' + payload['elevation'] + ")",
+            payload['power'],
+            (payload['shift_state'] && payload['shift_state'] !== '' ? payload['shift_state'] : 'O'),
+            payload['range'],
+            payload['est_range'],
+            now,
+            now
+          ],
+        function(err, result) {
+          if (err) console.error('Write failed due to %s', err);
+          else console.log('wrote to postgres: %s', result);
+          process_records();
+        });
+      } catch(e) {
+        if (err) console.error('lambda exception %s', e);
         process_records();
-      });
+      }
     });
   });
 };
--- ui/tesla/app/models/trip.rb
+++ ui/tesla/app/models/trip.rb
@@ -2,14 +2,4 @@ class Trip < ActiveRecord::Base
 
   has_many :vehicle_telemetry_metrics
 
-  def change
-    create_table :trips do |t|
-      t.string :vehicle_id
-      t.timestamp :start
-      t.timestamp :end
-    end
-
-    add_index :trips, :vehicle_id
-
-
 end
