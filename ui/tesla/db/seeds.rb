888ec41cabb91b6bb30ee2078101a2acf8eb483b
Ryan Breen <rbreen@cimpress.com>
2015-06-11 13:19:45 -0400

    Seed my vehicle.

--- ui/tesla/db/seeds.rb
+++ ui/tesla/db/seeds.rb
@@ -1,4 +1,4 @@
-
+=begin
 connection = ActiveRecord::Base.connection()
 connection.execute("insert into vehicle_telemetry_metrics (timestamp, vehicle_id, location, created_at, updated_at) (
 select now() as timestamp, i as vehicle_id, ST_GeographyFromText('SRID=4326;POINT(' || 39 + x.lon || ' ' || x.lat || ')'), now(), now()
@@ -12,4 +12,10 @@ VehicleTelemetryMetric.create(
   timestamp:      0,
   vehicle_id:     "1001", 
   location:       RGeo::Geographic.spherical_factory(srid: 4326).point(-75.990000, 39.010000)
+)
+=end
+
+Vehicle.create(
+  vehicle_id: "6856198612260671482",
+  name: "KITT"
 )
