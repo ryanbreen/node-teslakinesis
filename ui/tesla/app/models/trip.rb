f5d8ed73ad560556a8730ca2b0a8b90aac2a41a0
Ryan Breen <rbreen@cimpress.com>
2015-06-19 11:41:08 -0400

    Properly display named locations.

--- ui/tesla/app/models/trip.rb
+++ ui/tesla/app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ActiveRecord::Base
 
   belongs_to :vehicle
   has_many :vehicle_telemetry_metrics
-  has_one :origin, :class_name => "Location", :foreign_key => "start_location_id"
-  has_one :destination, :class_name => "Location", :foreign_key => "end_location_id"
+  has_one :origin, :class_name => "Location", :primary_key => 'start_location_id', :foreign_key => 'id'
+  has_one :destination, :class_name => "Location", :primary_key => 'end_location_id', :foreign_key => 'id'
 
 end
