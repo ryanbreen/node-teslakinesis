b79b4114f402d3392233e70bcf29d5b753641951
Ryan Breen <rbreen@cimpress.com>
2015-06-21 23:34:11 -0400

    Move trips to nested routes under vehicles.

--- ui/tesla/config/routes.rb
+++ ui/tesla/config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :vehicles do
     resources :trips, only: [:index]
     resources :locations
-  end
+    resources :trips
 
-  resources :trips, only: [:show], :as => :trip
-  get 'vehicles/:vehicle_id/trips/from/:from/to/:to', to: 'trips#between', as: 'vehicle_trips_between'
-  get 'vehicles/:vehicle_id/trips/from/:from', to: 'trips#from', as: 'vehicle_trips_from'
-  get 'vehicles/:vehicle_id/trips/to/:to', to: 'trips#to', as: 'vehicle_trips_to'
+    get 'trips/from/:from/to/:to', to: 'trips#between', as: 'vehicle_trips_between'
+    get 'trips/from/:from', to: 'trips#from', as: 'vehicle_trips_from'
+    get 'trips/to/:to', to: 'trips#to', as: 'vehicle_trips_to'
+  end
 
 end
