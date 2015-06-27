5d11d2e52182d01d4f219f919ceadd80eda4414e
Ryan Breen <rbreen@cimpress.com>
2015-06-26 22:46:12 -0400

    Paginate location

--- ui/tesla/app/controllers/locations_controller.rb
+++ ui/tesla/app/controllers/locations_controller.rb
@@ -7,7 +7,7 @@ class LocationsController < ApplicationController
   # GET /locations
   # GET /locations.json
   def index
-    @locations = Location.all
+    @locations = Location.all.paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /locations/1
