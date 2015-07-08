959bebcde67b398f991fa31820e4ba116a4845cb
Ryan Breen <rbreen@cimpress.com>
2015-07-07 21:00:50 -0400

    Fix docker build to use the cache more.

--- ui/build_and_deploy.sh
+++ ui/build_and_deploy.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 docker build -t ryanbreen/teslaui .
-docker push ryanbreen/teslaui
+until docker push ryanbreen/teslaui; do echo "Docker push failed, trying again..."; done
 
 # Grab id of the new task def
 ID=`aws ecs register-task-definition --cli-input-json file://teslaui.json --region us-east-1 | grep taskDefinitionArn | cut -d : -f 8 | cut -d \" -f 1`
