d1825fb53f9c11295f8ad2d06b658230edda486a
Ryan Breen <rbreen@cimpress.com>
2015-07-12 01:57:58 -0400

    Fix vehicles page.

--- ui/app/controllers/vehicles_controller.rb
+++ ui/app/controllers/vehicles_controller.rb
@@ -64,6 +64,7 @@ class VehiclesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vehicle
+      params[:vehicle_id] = params[:id]
       @vehicle = Vehicle.find(params[:vehicle_id])
     end
 
