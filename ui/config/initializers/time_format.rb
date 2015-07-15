09797679b42f98e0bb47d64ea520120b29e75fd7
Ryan Breen <rbreen@cimpress.com>
2015-07-15 18:05:00 -0400

    Reinstate telemetry.

--- ui/config/initializers/time_format.rb
+++ ui/config/initializers/time_format.rb
@@ -1,6 +1,7 @@
 Time::DATE_FORMATS[:long_us] = '%A %B %d, %Y %l:%M%p'
 Time::DATE_FORMATS[:date_us] = '%A %B %d, %Y'
 Time::DATE_FORMATS[:time_us] = '%l:%M%p'
+Time::DATE_FORMATS[:time_precise_us] = '%l:%M:%S.%L %p'
 
 require 'action_view'
 require 'action_view/helpers'
