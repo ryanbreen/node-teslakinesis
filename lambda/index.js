d0436a06ca459bea6bea9ba182796206a9c62b22
Ryan Breen <rbreen@cimpress.com>
2015-07-20 23:11:12 -0400

    Change tesla stream db.

--- lambda/index.js
+++ lambda/index.js
@@ -27,7 +27,7 @@ exports.handler = function(event, context) {
 
   if (event.Records.length === 0) return context.succeed();
 
-  var conn_string = "postgres://" + creds.DB_USER + ":" + creds.DB_PASSWORD + "@tesla.cfwzoyel2syn.us-east-1.rds.amazonaws.com/tesla";
+  var conn_string = "postgres://" + creds.DB_USER + ":" + creds.DB_PASSWORD + "@teslaprime.cfwzoyel2syn.us-east-1.rds.amazonaws.com/tesla";
   pg.connect(conn_string, function(err, client, done) {
 
     try {
