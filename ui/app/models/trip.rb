c1a956510721886a800fb9d4923212ce690ec56e
Ryan Breen <rbreen@cimpress.com>
2015-07-21 21:31:51 -0400

    Fix dependent syntax.

--- ui/app/models/trip.rb
+++ ui/app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ActiveRecord::Base
   has_merit
 
   belongs_to :vehicle
-  has_many :vehicle_telemetry_metrics, -> { order 'timestamp desc' }, :dependent => :delete
+  has_many :vehicle_telemetry_metrics, -> { order 'timestamp desc' }, :dependent => :delete_all
   has_one :origin, :class_name => "Location", :primary_key => 'start_location_id', :foreign_key => 'id'
   has_one :destination, :class_name => "Location", :primary_key => 'end_location_id', :foreign_key => 'id'
 
