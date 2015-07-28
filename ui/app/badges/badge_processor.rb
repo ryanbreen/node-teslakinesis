45c49a9594cf5145d95f25c21ac5f46253316520
Ryan Breen <rbreen@cimpress.com>
2015-07-27 22:29:42 -0400

    Move more logic into parent processor to keep children slim.

--- ui/app/badges/badge_processor.rb
+++ ui/app/badges/badge_processor.rb
@@ -1,5 +1,26 @@
 class BadgeProcessor
+  @badge_type_id = 0
+  @finalized = false
+
   def initialize(trip_detail)
     @trip_detail = trip_detail
+  end
+
+  def self.badge_type_id
+    @badge_type_id
+  end
+
+  def self.badge_type_id=(id)
+    @badge_type_id = id
+  end
+
+  def finalize(metric)
+    Badge.create(
+      :trip_id => @trip_detail.trip_id,
+      :trip_detail_id => @trip_detail.id,
+      :vehicle_telemetry_metric_id => metric.id,
+      :badge_type_id => self.class.badge_type_id
+    )
+    @finalized = true
   end
 end
--- ui/app/badges/speed_demon_badge_processor.rb
+++ ui/app/badges/speed_demon_badge_processor.rb
@@ -1,22 +1,13 @@
 class SpeedDemonBadgeProcessor < BadgeProcessor
 
+  self.badge_type_id = 1
+
   def process_metric(metric)
     return if @finalized
 
-    if metric.speed > 40
-      puts "Found a sweet metric speed."
+    if metric.speed > 90
       finalize metric
     end
-  end
-
-  def finalize(metric)
-    Badge.create(
-      :trip_id => @trip_detail.trip_id,
-      :trip_detail_id => @trip_detail.id,
-      :vehicle_telemetry_metric_id => metric.id,
-      :badge_type_id => 1
-    )
-    @finalized = true
   end
 
 end
