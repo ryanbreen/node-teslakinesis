db45dcb2b967b1f52e87912ec596eac7c3098c66
Ryan Breen <rbreen@cimpress.com>
2015-07-31 00:41:46 -0400

    Add pagination for detailed metrics.

--- ui/app/controllers/vehicle_telemetry_metrics_controller.rb
+++ ui/app/controllers/vehicle_telemetry_metrics_controller.rb
@@ -1,22 +1,13 @@
 class VehicleTelemetryMetricsController < ApplicationController
 
   def index
-    @vehicle_telemetry_metrics = VehicleTelemetryMetric.where("vehicle_id = ?", params[:id])
+    @vehicle_telemetry_metrics = VehicleTelemetryMetric.where("trip_id = ?", params[:trip_id]).
+      paginate(:page => params[:page], :per_page => 100)
 
-    lowest_lng, highest_lng, lowest_lat, highest_lat = nil
-
-    @hash = Gmaps4rails.build_markers(@vehicle_telemetry_metrics) do |vehicle, marker|
-      lowest_lat = vehicle.location.latitude if !lowest_lat || vehicle.location.latitude < lowest_lat
-      lowest_lng = vehicle.location.longitude if !lowest_lng || vehicle.location.longitude < lowest_lng
-      highest_lat = vehicle.location.latitude if !highest_lat || vehicle.location.latitude > highest_lat
-      highest_lng = vehicle.location.longitude if !highest_lng || vehicle.location.longitude > highest_lat
-
-      marker.lat vehicle.location.latitude
-      marker.lng vehicle.location.longitude
+    respond_to do |format|
+      format.html
+      format.json { render json: @vehicle_telemetry_metrics}
     end
-
-    @upper_left = { :lat => highest_lat, :lng => lowest_lng }
-    @lower_right = { :lat => lowest_lat, :lng => highest_lng }
   end
 
 
--- ui/config/routes.rb
+++ ui/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get 'trips/from/:from/to/:to', to: 'trips#between', as: 'trips_between', :constraints => { :from => /[^\/]+/, :to => /[^\/]+/ }
     get 'trips/from/:from', to: 'trips#from', as: 'trips_from', :constraints => { :from => /[^\/]+/ }
     get 'trips/to/:to', to: 'trips#to', as: 'trips_to', :constraints => { :to => /[^\/]+/ }
+    get 'vehicle_telemetry_metrics/:trip_id', to: 'vehicle_telemetry_metrics#index'
   end
 
 end
