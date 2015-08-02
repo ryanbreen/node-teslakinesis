f0d1ff59223f9ea4b4b98b688247ccf61db523b3
Ryan Breen <rbreen@cimpress.com>
2015-08-01 23:32:46 -0400

    Hide markers on multi-maps for now.

--- ui/app/assets/javascripts/custom-google-map-marker.js
+++ ui/app/assets/javascripts/custom-google-map-marker.js
@@ -1,59 +1,47 @@
-function CustomMarker(latlng, map, args) {
-	this.latlng = latlng;	
-	this.args = args;	
-	this.setMap(map);	
+function CustomMarker(latlng, map, div) {
+  this.latlng = latlng; 
+  this.div = div;
+  this.map = map;
+  this.setMap(map); 
 }
 
 CustomMarker.prototype = new google.maps.OverlayView();
 
 CustomMarker.prototype.draw = function() {
-	
-	var self = this;
-	
-	var div = this.div;
-	
-	if (!div) {
-	
-		div = this.div = document.createElement('div');
-		
-		div.className = 'marker';
-		
-		div.style.position = 'absolute';
-		div.style.cursor = 'pointer';
-		div.style.width = '20px';
-		div.style.height = '20px';
-		div.style.background = 'blue';
-		
-		if (typeof(self.args.marker_id) !== 'undefined') {
-			div.dataset.marker_id = self.args.marker_id;
-		}
-		
-		google.maps.event.addDomListener(div, "click", function(event) {
-			alert('You clicked on a custom marker!');			
-			google.maps.event.trigger(self, "click");
-		});
-		
-		var panes = this.getPanes();
-		panes.overlayImage.appendChild(div);
-	}
-	
-	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-	
-	if (point) {
-		div.style.position = 'absolute';
-		div.style.cursor = 'pointer';
-		div.style.left = (point.x - 5) + 'px';
-		div.style.top = (point.y - 6) + 'px';
-	}
+  
+  var self = this;
+  
+  var div = this.div;
+
+  //console.log(self.map);
+
+  var recalculate_overlay = function() {
+  
+    var point = self.getProjection().fromLatLngToContainerPixel(self.latlng);
+    //var point = self.getProjection().fromLatLngToDivPixel(self.latlng);
+    //console.log(point);
+  
+    if (point) {
+      div.style.position = 'absolute';
+      div.style.cursor = 'pointer';
+      div.style.left = (point.x - 5) + 'px';
+      div.style.top = (point.y - 6) + 'px';
+    }
+  };
+
+  // Wait for idle map
+  google.maps.event.addListener(this.map, 'idle', recalculate_overlay);
+  google.maps.event.addListener(this.map, 'bounds_changed', recalculate_overlay);
+  google.maps.event.addListener(window, 'load', recalculate_overlay);
 };
 
 CustomMarker.prototype.remove = function() {
-	if (this.div) {
-		this.div.parentNode.removeChild(this.div);
-		this.div = null;
-	}	
+  if (this.div) {
+    this.div.parentNode.removeChild(this.div);
+    this.div = null;
+  } 
 };
 
 CustomMarker.prototype.getPosition = function() {
-	return this.latlng;	
+  return this.latlng; 
 };
