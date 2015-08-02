52da219e99c5f7e626ad56f8e6a724a6f2e6a0ad
Ryan Breen <rbreen@cimpress.com>
2015-08-02 13:50:54 -0400

    Switch to public docker image.

--- ui/build_and_deploy.sh
+++ ui/build_and_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build -t ryanbreen/teslaui .
-until docker push ryanbreen/teslaui; do echo "Docker push failed, trying again..."; done
+docker build -t ryanbreen/teslakinesis-ui .
+until docker push ryanbreen/teslakinesis-ui; do echo "Docker push failed, trying again..."; sleep 1; done
 
 # Grab id of the new task def
 ID=`aws ecs register-task-definition --cli-input-json file://teslaui.json --region us-east-1 | grep taskDefinitionArn | cut -d : -f 8 | cut -d \" -f 1`
