d4a2faea2e35e7e830cfa81fddf5a0122a4f35c1
Ryan Breen <rbreen@cimpress.com>
2015-08-02 14:12:41 -0400

    Restore overlays on non-detailed maps.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd ui
 docker build -t ryanbreen/teslakinesis-ui .
 until docker push ryanbreen/teslakinesis-ui; do echo "Docker push failed, trying again..."; sleep 1; done
 
+cd ..
 # Grab id of the new task def
 ID=`aws ecs register-task-definition --cli-input-json file://teslaui.json --region us-east-1 | grep taskDefinitionArn | cut -d : -f 8 | cut -d \" -f 1`
 aws ecs update-service --service teslaui --task-definition teslakinesis-ui:$ID
