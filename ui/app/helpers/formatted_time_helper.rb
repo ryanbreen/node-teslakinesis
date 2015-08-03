e419732bcb0f9f5bb2cea2d8c80f96e093703214
Ryan Breen <rbreen@cimpress.com>
2015-08-02 22:37:20 -0400

    Move more logic into helpers.

--- ui/app/helpers/formatted_time_helper.rb
+++ ui/app/helpers/formatted_time_helper.rb
@@ -9,9 +9,12 @@ module FormattedTimeHelper
         trip.start_time.in_time_zone('America/New_York').to_formatted_s(:date_us)
   end
 
-  def pretty_duration(trip)
+  def pretty_duration(trip, summarize = false)
     if trip.end_time != nil
+      return "in #{distance_of_time_in_words(trip.start_time, trip.end_time, include_seconds: true)}" if summarize
       distance_of_time_in_words(trip.start_time, trip.end_time, include_seconds: true)
+    elsif summarize
+      "is ongoing"
     else
       distance_of_time_in_words(trip.start_time, DateTime.now, include_seconds: true)
     end
--- ui/app/helpers/trip_helper.rb
+++ ui/app/helpers/trip_helper.rb
@@ -24,4 +24,55 @@ module TripHelper
     end
   end
 
+  def from_name_short(trip)
+    trip.origin != nil ? trip.origin.name : "unknown origin"
+  end
+
+  def from_name(trip)
+    (trip.origin != nil ? trip.origin.name : "unknown origin (" + 
+      link_to('Name it!', new_vehicle_location_path(:vehicle_id => params[:vehicle_id], 
+        :lng => trip[:start_location].longitude, :lat => trip[:start_location].latitude, 
+        :z => trip[:start_location].z)) + ")").html_safe
+  end
+
+  def from_name_linked(trip)
+    (trip.origin != nil ? 
+      link_to(trip.origin.name, vehicle_location_path(trip.vehicle_id, trip.origin)) :
+      "unknown origin (#{link_to('Name it!', new_vehicle_location_path(:vehicle_id => params[:vehicle_id], 
+        :lng => trip[:start_location].longitude, :lat => trip[:start_location].latitude, 
+        :z => trip[:start_location].z))})").html_safe
+  end
+
+  def to_name_short(trip)
+    trip.destination != nil ? trip.destination.name : "unknown destination"
+  end
+
+  def to_name(trip)
+    (trip.destination != nil ? trip.destination.name : "unknown destination (" + 
+      link_to('Name it!', new_vehicle_location_path(:vehicle_id => params[:vehicle_id], 
+        :lng => trip[:end_location].longitude, :lat => trip[:end_location].latitude, 
+        :z => trip[:end_location].z)) + ")").html_safe unless trip[:end_location] == nil
+  end
+
+  def to_name_linked(trip)
+    (trip.destination != nil ?
+      link_to(trip.destination.name, vehicle_location_path(trip.vehicle_id, trip.destination)) :
+      "unknown destination (" + 
+      link_to('Name it!', new_vehicle_location_path(:vehicle_id => params[:vehicle_id], 
+        :lng => trip[:end_location].longitude, :lat => trip[:end_location].latitude, 
+        :z => trip[:end_location].z)) + ")" unless trip[:end_location] == nil).html_safe
+  end
+
+  def trip_between_summary(trip)
+    if trip.end_time == nil
+      ("Ongoing trip from #{from_name(trip)}").html_safe
+    else
+      ("Trip from #{from_name(trip)} to #{to_name(trip)}").html_safe
+    end
+  end
+
+  def trip_destination_summary(trip)
+    (trip.end_time == nil ? 'ongoing' : to_name_linked(trip)).html_safe
+  end
+
 end
