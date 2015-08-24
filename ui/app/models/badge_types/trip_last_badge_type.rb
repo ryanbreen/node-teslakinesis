99558ce4fae8231216d2e17a7015a1531509089a
Ryan Breen <rbreen@cimpress.com>
2015-08-24 01:07:08 -0400

    Pretty up trip position badge display.

--- ui/app/models/badge_types/trip_last_badge_type.rb
+++ ui/app/models/badge_types/trip_last_badge_type.rb
@@ -1,7 +1,7 @@
 class TripLastBadgeType < BadgeType
 
   def to_description(badge)
-    "The total duration of the trip was #{badge.data}s."
+    "The total duration of the trip was #{distance_of_time_in_words badge.data.to_i}."
   end
 
 end
