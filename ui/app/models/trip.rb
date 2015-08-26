5a35d400f3adfc5cbea935d3e180de870932299c
Ryan Breen <rbreen@cimpress.com>
2015-08-26 12:58:15 -0400

    Remove trip.badges relation since it just caused confusion.

--- ui/app/models/trip.rb
+++ ui/app/models/trip.rb
@@ -2,7 +2,6 @@ class Trip < ActiveRecord::Base
   belongs_to :vehicle
   has_many :vehicle_telemetry_metrics, -> { order 'timestamp desc' }, :dependent => :delete_all
   has_one :trip_detail, :dependent => :delete
-  has_many :badges, through: :trip_detail 
   has_one :origin, :class_name => "Location", :primary_key => 'start_location_id', :foreign_key => 'id'
   has_one :destination, :class_name => "Location", :primary_key => 'end_location_id', :foreign_key => 'id'
 
