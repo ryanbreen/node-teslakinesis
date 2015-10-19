cebc9d34985e42c911a9f7339508e8a925a69ee2
Ryan Breen <rbreen@cimpress.com>
2015-10-18 23:49:23 -0400

    Add badge base.

---
+++ lambda/data_processor/badges/badge.js
@@ -0,0 +1,27 @@
+
+var Badge = function() {
+  this.last_issued = 4611686018427387903;
+};
+
+Badge.create = function(trip_detail, metric, data) {
+  if (!metric) return;
+
+  // Check whether the last time we issued this badge was less than a minute ago.  We do this
+  // so similar badges don't stack up on each other.
+  if ((this.last_issued - metric.timestamp) < 60) return;
+
+  this.last_issued = metric.timestamp
+
+/**
+  Badge.create(
+    :vehicle_id => trip_detail.vehicle_id,
+    :trip_id => trip_detail.trip_id,
+    :trip_detail_id => trip_detail.id,
+    :vehicle_telemetry_metric_id => metric.id,
+    :badge_type_id => id,
+    :data => data
+  )
+**/
+};
+
+module.exports = Badge;
