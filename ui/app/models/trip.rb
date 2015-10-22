2caca2755d0778d059999fd561ff9eb2b794fa1a
Ryan Breen <rbreen@cimpress.com>
2015-10-22 17:13:43 -0400

    Trip detail calculations are now handled by lambda, so remove them from Rails.

--- ui/app/models/trip.rb
+++ ui/app/models/trip.rb
@@ -5,12 +5,4 @@ class Trip < ActiveRecord::Base
   has_one :origin, :class_name => "Location", :primary_key => 'start_location_id', :foreign_key => 'id'
   has_one :destination, :class_name => "Location", :primary_key => 'end_location_id', :foreign_key => 'id'
 
-  # For detailed routes, we want to use line segments of different colors to represent different speeds.
-  # We use green for 0-25MPH, orange for 25-50MPH, and red for 50+.
-  @@color_scale = [
-    "#74AD6A",
-    "#FFAA38",
-    "#C44537"
-  ]
-
 end
