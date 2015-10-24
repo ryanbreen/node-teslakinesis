ac215c3e8a15a8fdd8301452599fd772cbd673d8
Ryan Breen <rbreen@cimpress.com>
2015-10-24 14:26:48 -0400

    Begin adding a clojure-based lambda function for trip data.

--- lambda/get_trips/project.clj
+++ lambda/get_trips/project.clj
@@ -1,6 +1,8 @@
 (defproject get-trips "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.amazonaws/aws-lambda-java-core "1.0.0"]]
+                 [com.amazonaws/aws-lambda-java-core "1.0.0"]
+                 [amazonica "0.3.35"]
+                 [clj-time "0.11.0"]]
   :java-source-paths ["src/java"]
   :aot :all)
--- lambda/get_trips/src/trips.clj
+++ lambda/get_trips/src/trips.clj
@@ -1,6 +1,28 @@
 (ns trips
   (:gen-class
-   :methods [^:static [handler [String] String]]))
+   :methods [^:static [get [] String]])
+  (:use [clojure.java.io :as io]
+        [clojure.data.json :as json]
+        [amazonica.aws.kinesis]))
 
-(defn -get [s]
-  (str "Hello " s "!"))
+(defn my-value-writer [key value]
+  (if (= key :attach-time)
+    (str (org.joda.time.DateTime. (.toString value)))
+    value)
+  (if (= key :launch-time)
+    (str (org.joda.time.DateTime. (.toString value)))
+    value))
+
+(defn creds []
+  (read-string
+    (slurp
+      (io/file
+        (io/resource
+          "creds/aws.creds" )))))
+
+(defn -get []
+  (json/write-str (list-streams (creds)) :value-fn my-value-writer))
+  ;(print-str (describe-instances cred) :value-fn my-value-writer))
+
+(println (-get))
+;(pprint (-get))
