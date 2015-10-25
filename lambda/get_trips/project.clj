750eb92f8d978c6e82c4d1dac3036fec86a0fee7
Ryan Breen <rbreen@cimpress.com>
2015-10-25 00:24:00 -0400

    Add postgres query support to get_trips.

--- lambda/get_trips/project.clj
+++ lambda/get_trips/project.clj
@@ -1,8 +1,7 @@
 (defproject get-trips "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.amazonaws/aws-lambda-java-core "1.0.0"]
-                 [amazonica "0.3.35"]
-                 [clj-time "0.11.0"]]
+                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4-1204-jdbc41"]]
   :java-source-paths ["src/java"]
   :aot :all)
--- lambda/get_trips/src/trips.clj
+++ lambda/get_trips/src/trips.clj
@@ -3,26 +3,17 @@
    :methods [^:static [get [] String]])
   (:use [clojure.java.io :as io]
         [clojure.data.json :as json]
-        [amazonica.aws.kinesis]))
-
-(defn my-value-writer [key value]
-  (if (= key :attach-time)
-    (str (org.joda.time.DateTime. (.toString value)))
-    value)
-  (if (= key :launch-time)
-    (str (org.joda.time.DateTime. (.toString value)))
-    value))
+        [clojure.java.jdbc :as sql]))
 
 (defn creds []
-  (read-string
-    (slurp
-      (io/file
-        (io/resource
-          "creds/aws.creds" )))))
+  (slurp
+    (io/file
+      (io/resource
+        "creds/db.creds" ))))
 
 (defn -get []
-  (json/write-str (list-streams (creds)) :value-fn my-value-writer))
-  ;(print-str (describe-instances cred) :value-fn my-value-writer))
+  (sql/query (creds)
+    ["select * from trips limit 10;"]))
 
-;(println (-get))
+(println (-get))
 ;(pprint (-get))
