cef51cfa1a9289b4b487433451fb3357788183d6
Ryan Breen <rbreen@cimpress.com>
2015-11-16 16:53:28 -0500

    Properly output stream results.

--- lambda/get_trips/src/trips.clj
+++ lambda/get_trips/src/trips.clj
@@ -18,10 +18,12 @@
     :else value))
 
 (defn -handleRequest [this is os context]
-  (println (str this))
-  (let [db_creds (creds)]
-    (let [result (sql/query db_creds
-      ["select start_time, start_location_id, st_asgeojson(start_location) as start_location, end_time, end_location_id, st_asgeojson(end_location) as end_location from trips limit 10;"])]
-      (json/read-str
-        (json/write-str result
-          :value-fn my-value-writer)))))
+  (let [w (io/writer os)]
+    (println (str this))
+    (let [db_creds (creds)]
+      (let [result (sql/query db_creds
+        ["select start_time, start_location_id, st_asgeojson(start_location) as start_location, end_time, end_location_id, st_asgeojson(end_location) as end_location from trips limit 10;"])]
+        (let [w (io/writer os)]
+          (-> (json/read-str (json/write-str result :value-fn my-value-writer))
+              (json/write w))
+          (.flush w))))))
