8c8012697c3faf4ffcd39fe2f3cf287f5941fa85
Ryan Breen <rbreen@cimpress.com>
2015-12-09 01:20:54 -0500

    Remove some clojure projects.

--- lambda/get_trips/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject get-trips "0.1.0"
-  :dependencies [[com.amazonaws/aws-lambda-java-core "1.0.0"]
-                 [org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/java.jdbc "0.4.1"]
-                 [org.postgresql/postgresql "9.4-1204-jdbc41"]]
-  :java-source-paths ["src/java"]
-  :aot :all)
