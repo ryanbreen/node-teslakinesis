6e257a6815c3d2b4d96fc8269ba29aebb22b25aa
Ryan Breen <rbreen@cimpress.com>
2016-05-22 20:41:39 -0400

    Fix missing js.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -5,5 +5,5 @@ until docker push ryanbreen/teslakinesis-ui; do echo "Docker push failed, trying
 
 cd ..
 # Grab id of the new task def
-ID=`aws ecs register-task-definition --cli-input-json file://teslaui.json --region us-east-1 | grep taskDefinitionArn | cut -d : -f 8 | cut -d \" -f 1`
-aws ecs update-service --service teslaui --task-definition teslakinesis-ui:$ID
+ID=`aws --profile ryanbreen ecs register-task-definition --cli-input-json file://teslaui.json --region us-east-1 | grep taskDefinitionArn | cut -d : -f 8 | cut -d \" -f 1`
+aws --profile ryanbreen ecs update-service --service teslaui --task-definition teslakinesis-ui:$ID
