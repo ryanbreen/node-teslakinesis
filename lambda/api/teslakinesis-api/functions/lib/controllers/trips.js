6f1786ce883e60c68773a12a30b0d8141c476987
Ryan Breen <rbreen@cimpress.com>
2016-06-20 21:36:50 -0400

    Join and display trip detail

--- lambda/api/teslakinesis-api/functions/lib/controllers/trips.js
+++ lambda/api/teslakinesis-api/functions/lib/controllers/trips.js
@@ -6,7 +6,8 @@
  *   - payload: a parameter to pass to the operation being performed
  */
 
-var Trip = require('../models/trip');
+var Trip = require('../models/trip.js');
+var TripDetail = require('../models/trip_detail.js');
 
 module.exports.respond = function(event, cb) {
 
@@ -16,11 +17,22 @@ module.exports.respond = function(event, cb) {
     case 'GET':
       if (event.id) {
         console.log("Looking for trip %s for vehicle %s", event.id, event.vehicle_id);
-        Trip.findOne({ where: { vehicle_id : event.vehicle_id, id: event.id } }).then(function (trip) {
+        Trip.findOne({
+          where: { vehicle_id : event.vehicle_id, id: event.id },
+          include: [{
+            model: TripDetail
+          }]
+        }).then(function (trip) {
           return cb(null, trip);
         });
       } else {
-        Trip.findAll({ where: { vehicle_id : event.vehicle_id }, order: 'id DESC' }).then(function (trips) {
+        Trip.findAll({
+          where: { vehicle_id : event.vehicle_id },
+          order: 'id DESC',
+          include: [{
+            model: TripDetail
+          }]
+        }).then(function (trips) {
           return cb(null, trips);
         });
       }
---
+++ lambda/api/teslakinesis-api/functions/lib/models/trip_detail.js
@@ -0,0 +1,28 @@
+var DB_CREDS = require('../creds/db.js');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(DB_CREDS.URI);
+
+var Trip = require('./trip.js');
+
+var TripDetail = module.exports = sequelize.define('trip_detail', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  vehicle_id: Sequelize.CHAR,
+  trip_id: Sequelize.INTEGER,
+  detailed_route: Sequelize.TEXT,
+  summary_route: Sequelize.TEXT,
+  upper_left: Sequelize.TEXT,
+  lower_right: Sequelize.TEXT,
+  created_at: Sequelize.TIME,
+  updated_at: Sequelize.TIME,
+  true_duration: Sequelize.INTEGER
+}, {
+  tableName: 'trip_details',
+  timestamps: false
+});
+
+TripDetail.belongsTo(Trip, { foreignKey : 'trip_id' });
+Trip.hasOne(TripDetail, { foreignKey : 'trip_id' });
